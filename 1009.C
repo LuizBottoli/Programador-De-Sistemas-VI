8836feb35b622ae416f739e324d1e3009b95366e
LuizBottoli <Bottoli533@gmail.com>
2026-06-15 16:59:41 -0300

    Novos Exercico

---
+++ 1009.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char nome[50];
+    double salario_fixo, total_vendas, salario_final;
+
+
+    scanf("%s", nome);
+    scanf("%lf", &salario_fixo);
+    scanf("%lf", &total_vendas);
+
+    salario_final = salario_fixo + (total_vendas * 0.15);
+
+    printf("TOTAL = R$ %.2lf\n", salario_final);
+
+    return 0;
+}
